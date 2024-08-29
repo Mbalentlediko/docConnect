fc1b82c495364533ec19ae685e368e94965c52c4
Mbalentlediko <dikombalentle9@gmail.com>
2024-08-29 13:45:25 +0200

    appointments

--- model/appointments.js
+++ model/appointments.js
@@ -2,7 +2,14 @@ import { compare,hash } from "bcrypt";
 
 class appointments{
     fetchAppointments(req,res) {
-    
-        }
+        // try {
+        //     const strQry = `
+        //     SELECT  appointment_id,
+        //     patient_id,service_id,
+        //     doctor_id,appointment_date,
+        //     total_price,status
+        //     FROM appointments`
+        //     connection.qury
+        // }
     }
-export {appointments}
+}
