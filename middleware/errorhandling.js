274a8485919e1fc5cc44e8fca883f65e9a278a99
Mbalentlediko <dikombalentle9@gmail.com>
2024-08-31 11:28:46 +0200

    errorHandling

---
+++ middleware/errorhandling.js
@@ -0,0 +1,11 @@
+function errorHandling(err,req,res,next){
+    if (err || res.statusCode >=400 ) {
+        res.json({
+            status:err.statusCode ||500,
+            err: "An error occured, Please try again later"
+        })
+    }
+        next()
+
+    }
+        export {errorHandling}
